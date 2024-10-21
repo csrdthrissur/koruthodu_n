51b8cf8d1eb8d3c140dee50230bee645bb939df8
csrdthrissur <155517405+csrdthrissur@users.noreply.github.com>
2024-10-21 20:03:35 +0200

    Add files via upload

---
+++ data/BoreWell_16.js
@@ -0,0 +1,1 @@
+var json_BoreWell_16 = {"type":"FeatureCollection","name":"BoreWell_16","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Id":0.0,"Name":"Bore Well","Ward":"5","Code":"W5B01","Road_Acess":"Muppathilpadi Anganwadi Road"},"geometry":{"type":"Point","coordinates":[76.969515223761476,9.472113528033098]}}]}
